333e3c95de13c1690848b0cd98885c9f4950f338
SUFandom <bluepearl394@gmail.com>
2023-12-19 17:38:08 +0800

    Revert "come yadayada"
    This reverts commit 0eb49bd1ec358c10eebd6893b8f3368da8b52814.

--- twrp_a12s.mk
+++ twrp_a12s.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # FIX FOR TWRP CRYING ABOUT 64BIT SHID
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 # Stock Pull IN
-# Disabling Full base telephonyals
+# Disabling Full base telephony
 # $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
