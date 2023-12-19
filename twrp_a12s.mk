68214252267e13e54910366c5164ae1a02fa5c5c
SUFandom <bluepearl394@gmail.com>
2023-12-19 11:35:22 +0800

    change in  omni to pb

--- twrp_a12s.mk
+++ twrp_a12s.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
 # # Inherit some common PitchBlack stuff.
-# $(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
