6180cbb65c39f954fc1667dae3351eedc77a4f5b
SUFandom <bluepearl394@gmail.com>
2023-12-19 11:52:58 +0800

    forgor to disable omni

--- twrp_a12s.mk
+++ twrp_a12s.mk
@@ -16,8 +16,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # # Inherit some common PitchBlack stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# # Inherit some common Omni stuff.
+# $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from a12s device
 $(call inherit-product, device/samsung/a12s/device.mk)
