39475843a9b3e9ec26d5c0ab91f3a2377d1f5b1e
SUFandom <bluepearl394@gmail.com>
2023-12-19 11:07:54 +0800

    pulling omni in to the table + removing TWRP Inclusions + DIsabling import of TOYBOX

--- twrp_a12s.mk
+++ twrp_a12s.mk
@@ -13,8 +13,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
-# Inherit some common PitchBlack stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+# # Inherit some common PitchBlack stuff.
+# $(call inherit-product, vendor/pb/config/common.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from a12s device
 $(call inherit-product, device/samsung/a12s/device.mk)
