8150820c10cd2597914c2f390a6e599f16adfb52
SUFandom <bluepearl394@gmail.com>
2023-12-19 11:02:44 +0800

    comment update

--- twrp_a12s.mk
+++ twrp_a12s.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
-# Inherit some common Omni stuff.
+# Inherit some common PitchBlack stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from a12s device
