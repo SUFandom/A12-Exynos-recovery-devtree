fdb38d51167967413e719eb35d7bd8fdb2563104
SUFandom <bluepearl394@gmail.com>
2023-12-20 09:11:48 +0800

    vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# FORCE TO LET THE SCRIPT TO TAKE NOTE THAT THERE IS ALREADY A KERNEL
+export OF_FORCE_PREBUILT_KERNEL=1
+
+add_lunch_combo twrp_a12s-eng
