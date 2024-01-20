56da25c460dd69eb35d3c28509fbe941c16a5b13
SUFandom <bluepearl394@gmail.com>
2024-01-21 06:28:34 +0800

    something something test

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,5 @@
 chmod a+x device/samsung/a12s/mkbootimg
+git clone https://gitlab.com/HayateDevTH/avb_tool -b main out/host/linux-x86/bin
+sudo chmod +rwx out/host/linux-x86/bin/avbtool
+chmod a+x device/samsung/a12s/mkbootimg
+add_lunch_combo twrp_a12s-eng
